0918e6b97aff3b9d0288d6749ee418a0478a86c9
Demo-019 <vikasvikisingh@gmail.com>
2026-04-27 00:41:41 +0530

    Day02 Welcome screen completed

--- Project01/MyCoffeeApp/app/src/main/java/com/example/mycoffeeapp/MainActivity.kt
+++ Project01/MyCoffeeApp/app/src/main/java/com/example/mycoffeeapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mycoffeeapp.screens.welcomescreen.WelcomeScreen
 import com.example.mycoffeeapp.ui.theme.MyCoffeeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyCoffeeAppTheme {
-
-                }
+                WelcomeScreen()
             }
         }
     }
+}
 
---
+++ Project01/MyCoffeeApp/app/src/main/java/com/example/mycoffeeapp/screens/welcomescreen/WelcomeScreen.kt
@@ -0,0 +1,90 @@
+package com.example.mycoffeeapp.screens.welcomescreen
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mycoffeeapp.R
+import com.example.mycoffeeapp.ui.theme.LightBrown
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black),
+    ) {
+        Image(
+            painter = painterResource(R.drawable.image_splash),
+            alignment = Alignment.TopEnd,
+            contentDescription = "Welcome Image",
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 70.dp, horizontal = 24.dp),
+            verticalArrangement = Arrangement.Bottom,
+            Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Fall in Love with Coffee in Blissful Delight!",
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 28.sp
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Welcome to our cozy coffee corner, where every cup is a delight for you.",
+                color = Color.LightGray,
+                fontSize = 15.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = LightBrown,
+                    contentColor = Color.White
+                )
+
+            ) {
+                Text(
+                    text = "Get Started",
+                    fontSize = 18.sp
+                )
+            }
+        }
+    }
+
+}
